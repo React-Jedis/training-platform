70300c8d83c3cafd8a1bc13fac4652ab9a007625
Rubén <rubenofen@gmail.com>
2020-06-14 01:27:50 +0200

    La llamada no la hace

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,4 @@
 import './src/css/style.css';
 import './src/css/global.css';
 
-import { wrapRootElement } from './gatsby-ssr';
-
-export { wrapRootElement };
+export { wrapRootElement } from './gatsby-ssr';
